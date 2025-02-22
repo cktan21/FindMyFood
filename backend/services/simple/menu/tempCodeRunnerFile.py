1e548dbb95f007529e4e6e3a36c37910579999eb
Kevin Tan <cekevintan@gmail.com>
2025-02-22 22:42:30 +0800

    Revert "order api done"
    This reverts commit 5fa0ef2f2fc104448f512431f9d81b3661eb77f0.

--- backend/services/simple/menu/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def get_photo_by_id(name):
-#     listie = []
-#     blob = bucket.list_blobs(prefix=name)
-#     for photo in blob:
-#         url = blob.public_url
-#         listie+=[url]
-#     return url
