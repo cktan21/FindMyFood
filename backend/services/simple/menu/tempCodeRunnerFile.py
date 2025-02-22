5fa0ef2f2fc104448f512431f9d81b3661eb77f0
Kevin Tan <cekevintan@gmail.com>
2025-02-22 22:29:27 +0800

    order api done

---
+++ backend/services/simple/menu/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+def get_photo_by_id(name):
+#     listie = []
+#     blob = bucket.list_blobs(prefix=name)
+#     for photo in blob:
+#         url = blob.public_url
+#         listie+=[url]
+#     return url
