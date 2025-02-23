893f2bc8589235cff0b6cd0bd04c80f40773ab9c
Kevin Tan <cekevintan@gmail.com>
2025-02-23 11:52:52 +0800

    menu fully functional

--- backend/services/simple/menu/photo.py
+++ backend/services/simple/menu/photo.py
@@ -12,8 +12,11 @@ def get_photo_by_id(name):
     listie = []
     blob = bucket.list_blobs(prefix=name)
     for photo in blob:
-        url = photo.public_url
+        file_path = photo.name
+        encoded_file_path = file_path.replace("/", "%2F")  # Encode slashes in the file path
+        url = f"https://firebasestorage.googleapis.com/v0/b/{bucket.name}/o/{encoded_file_path}?alt=media"
         listie+=[url]
     return listie[1:]
+
 
 # print(get_photo_by_id('King Kong Curry'))
