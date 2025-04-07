0bfd0b1f3ace0e3042a2cea5649e63e530da1e3e
Ryan Bangras <49177186+ryanbangras@users.noreply.github.com>
2025-04-07 14:07:48 +0800

    Added logo cloud of restaurants, simplified footer

--- frontend/src/components/blocks/Footer.tsx
+++ frontend/src/components/blocks/Footer.tsx
@@ -1,60 +1,6 @@
-import { Link } from "react-router-dom";
-
 export const Footer = () => {
-  const navigationItems = [
-    {
-      title: "Home",
-      href: "/",
-      description: "",
-    },
-    {
-      title: "Product",
-      description: "Managing a small business today is already tough.",
-      items: [
-        {
-          title: "Reports",
-          href: "/reports",
-        },
-        {
-          title: "Statistics",
-          href: "/statistics",
-        },
-        {
-          title: "Dashboards",
-          href: "/dashboards",
-        },
-        {
-          title: "Recordings",
-          href: "/recordings",
-        },
-      ],
-    },
-    {
-      title: "Company",
-      description: "Managing a small business today is already tough.",
-      items: [
-        {
-          title: "About us",
-          href: "/about",
-        },
-        {
-          title: "Fundraising",
-          href: "/fundraising",
-        },
-        {
-          title: "Investors",
-          href: "/investors",
-        },
-        {
-          title: "Contact us",
-          href: "/contact",
-        },
-      ],
-    },
-  ];
-
   return (
-    <div className="w-full py-20 lg:py-40 bg-foreground text-background bg-gradient-to-r from-blue-600 via-blue-400 to-purple-500">
+    <div className="w-full py-4 lg:py-8 bg-foreground text-background bg-gradient-to-r from-blue-600 via-blue-400 to-purple-500">
       <div className="container mx-auto">
         <div className="grid lg:grid-cols-2 gap-10 items-center">
           <div className="flex gap-8 flex-col items-start">
@@ -72,44 +18,7 @@ export const Footer = () => {
                 <p>Singapore Management University</p>
                 <p>188065</p>
               </div>
-              <div className="flex flex-col text-sm max-w-lg leading-relaxed tracking-tight text-background/75 text-left">
-                <Link to="/">Terms of service</Link>
-                <Link to="/">Privacy Policy</Link>
-              </div>
             </div>
-          </div>
-          <div className="grid lg:grid-cols-3 gap-10 items-start">
-            {navigationItems.map((item) => (
-              <div
-                key={item.title}
-                className="flex text-base gap-1 flex-col items-start"
-              >
-                <div className="flex flex-col gap-2">
-                  {item.href ? (
-                    <Link
-                      to={item.href}
-                      className="flex justify-between items-center"
-                    >
-                      <span className="text-xl">{item.title}</span>
-                    </Link>
-                  ) : (
-                    <p className="text-xl">{item.title}</p>
-                  )}
-                  {item.items &&
-                    item.items.map((subItem) => (
-                      <Link
-                        key={subItem.title}
-                        to={subItem.href}
-                        className="flex justify-between items-center"
-                      >
-                        <span className="text-background/75">
-                          {subItem.title}
-                        </span>
-                      </Link>
-                    ))}
-                </div>
-              </div>
-            ))}
           </div>
         </div>
       </div>
